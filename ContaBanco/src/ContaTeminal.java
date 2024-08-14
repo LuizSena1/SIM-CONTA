7e3e76747723b5fa05e5003dfa9e10215f317d8c
LuizSena1 <luizs.sena7@gmail.com>
2024-08-14 16:04:21 -0300

    commit

---
+++ ContaBanco/src/ContaTeminal.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ContaTeminal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Numero = sc.nextInt();
+        String Agencia = sc.next();
+        String NomeCliente1 = sc.nextLine();
+        String NomeCliente2 = sc.nextLine();
+        String NomeCliente = NomeCliente1 + " " + NomeCliente2;
+        double Saldo = sc.nextDouble();
+        System.out.format("Olá%s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",NomeCliente,Agencia,Numero,Saldo);
+    }
+}
