45334e506a75b279adb8106952011326801ff70b
LuizSena1 <luizs.sena7@gmail.com>
2024-08-14 16:08:51 -0300

    %n no final do print

--- ContaBanco/src/ContaTeminal.java
+++ ContaBanco/src/ContaTeminal.java
@@ -8,6 +8,6 @@ public class ContaTeminal {
         String NomeCliente2 = sc.nextLine();
         String NomeCliente = NomeCliente1 + " " + NomeCliente2;
         double Saldo = sc.nextDouble();
-        System.out.format("Olá%s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",NomeCliente,Agencia,Numero,Saldo);
+        System.out.format("Olá%s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque%n",NomeCliente,Agencia,Numero,Saldo);
     }
 }
