3a0429fc03bac25bc31f95200f7c0685f1c9ecb0
LuizSena1 <luizs.sena7@gmail.com>
2024-08-14 22:54:07 -0300

    mudança no NomeCompleto, retirando espaço

--- ContaBanco/src/ContaTeminal.java
+++ ContaBanco/src/ContaTeminal.java
@@ -6,8 +6,8 @@ public class ContaTeminal {
         String Agencia = sc.next();
         String NomeCliente1 = sc.nextLine();
         String NomeCliente2 = sc.nextLine();
-        String NomeCliente = NomeCliente1 + " " + NomeCliente2;
+        String NomeCliente = NomeCliente1 + NomeCliente2;
         double Saldo = sc.nextDouble();
-        System.out.format("Olá%s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque%n",NomeCliente,Agencia,Numero,Saldo);
+        System.out.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque%n",NomeCliente,Agencia,Numero,Saldo);
     }
 }
